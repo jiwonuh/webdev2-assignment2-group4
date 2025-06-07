61e179b49e37de013fcc1ae2467360d09c956eec
Harry-SAIT <chinglok.chu@edu.sait.ca>
2025-06-06 22:18:49 -0600

    updated Nabar.js

--- student-portal/components/Navbar.js
+++ student-portal/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center mt-2">
         <div className="text-lg font-bold">New Generation High Shool - Student Portal System</div>
         <ul className="flex space-x-10">
-          <li><a href="/" className="hover:underline">Home</a></li>
+          <li><a href="/home" className="hover:underline">Home</a></li>
           <li><a href="/courses" className="hover:underline">Courses</a></li>
           <li><a href="/profile" className="hover:underline">Profile</a></li>
         </ul>

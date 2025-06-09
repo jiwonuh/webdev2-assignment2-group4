2f58777e7a204cc835c33f88d95972462a679bbb
DongjeenHan <dongjeenhan@gmail.com>
2025-06-09 02:45:16 -0600

    Update Navbar.js

--- student-portal/components/Navbar.js
+++ student-portal/components/Navbar.js
@@ -1,5 +1,4 @@
 'use client';
-import Link from 'next/link';
 
 export default function Navbar() {
   return (
@@ -7,9 +6,9 @@ export default function Navbar() {
       <div className="flex justify-between items-center">
         <h1 className="text-xl font-bold">New Generation High School</h1>
         <ul className="flex space-x-6">
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/Student_List">Student List</Link></li>
-          <li><Link href="/Add_Student">Add Student</Link></li>
+          <li><a href="/">Home</a></li>
+          <li><a href="/Add_Student">Add Student</a></li>
+          <li><a href="/Student_List">Student List</a></li>
         </ul>
       </div>
     </nav>

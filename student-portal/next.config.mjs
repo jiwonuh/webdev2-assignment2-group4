791d137779eb5583ae530fa7b03909c7f7aae971
Jiwon <Jiwon.Uh@edu.sait.ca>
2025-06-08 20:47:49 -0600

    Update next.config.mjs

--- student-portal/next.config.mjs
+++ student-portal/next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+  output: 'export',
+  basePath: '/webdev2-assignment2-group4',
+  images: {
+    unoptimized: true,
+  },
 };
-
 
 export default nextConfig;

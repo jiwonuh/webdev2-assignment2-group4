0b13a2e215e6f6105f444bf1259383fbf56d6439
DongjeenHan <dongjeenhan@gmail.com>
2025-06-09 01:49:03 -0600

    Update Navbar.js

--- student-portal/components/Navbar.js
+++ student-portal/components/Navbar.js
@@ -1,17 +1,15 @@
 'use client';
 import Link from 'next/link';
 
-const basePath = '/webdev2-assignment2-group4'; 
-
 export default function Navbar() {
   return (
     <nav className="bg-blue-700 text-white px-6 py-4 ">
       <div className="flex justify-between items-center">
         <h1 className="text-xl font-bold">New Generation High School</h1>
         <ul className="flex space-x-6">
-          <li><Link href={`${basePath}/`}>Home</Link></li>
-          <li><Link href={`${basePath}/Student_List`}>Student List</Link></li>
-          <li><Link href={`${basePath}/Add_Student`}>Add Student</Link></li>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/Student_List">Student List</Link></li>
+          <li><Link href="/Add_Student">Add Student</Link></li>
         </ul>
       </div>
     </nav>

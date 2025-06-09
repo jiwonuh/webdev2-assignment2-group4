5a14614f6f0a5daadb759103bcbcd2642add1f4a
DongjeenHan <dongjeenhan@gmail.com>
2025-06-09 01:47:02 -0600

    Update next.config.mjs

--- student-portal/next.config.mjs
+++ student-portal/next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/webdev2-assignment2-group4',
   images: {
     unoptimized: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: '/webdev2-assignment2-group4',
   },
 };
 
